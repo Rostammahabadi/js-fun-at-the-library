0de65e16fe2cf87834f4cb0650b7b6f3aef04d94
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-08-30 13:33:55 -0600

    add shelf with appropriate functions

--- src/shelf.js
+++ src/shelf.js
@@ -1,10 +1,26 @@
 function shelfBook(book, shelf){
-  shelf.push(book)
+  if(shelf.length != 3){
+    shelf.unshift(book)
+  }
+}
+
+function unshelfBook(book, shelf){
+  var index = shelf.map(shelf=> shelf.title).indexOf(book)
+  shelf.splice(index, 1)
+}
+
+function listTitles(shelf){
+  return shelf.map(shelf=>shelf.title).join(", ")
+}
+
+function searchShelf(shelf, title){
+  var index = shelf.map(shelf=> shelf.title)
+  return index.includes(title)
 }
 
 module.exports = {
   shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
-  // searchShelf: searchShelf
+  unshelfBook: unshelfBook,
+  listTitles: listTitles,
+  searchShelf: searchShelf
 };
